2899ed6eb04c33daab03913b28c3a5c67675494f
BlinhNhi <trantoan1682016@gmail.com>
2024-09-23 01:19:13 +0700

    up feedback

---
+++ lib/ui/admin/feeedback/feedback_screen.dart
@@ -0,0 +1,68 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:techwiz_5/ui/admin/ambulance/create_ambulance.dart';
+import 'package:techwiz_5/ui/widgets/ambulance_card.dart';
+import 'package:techwiz_5/ui/widgets/feedback_card.dart';
+
+class FeedbackScreen extends StatefulWidget {
+  const FeedbackScreen({
+    super.key,
+  });
+
+  @override
+  State<FeedbackScreen> createState() => _FeedbackScreen();
+}
+
+class _FeedbackScreen extends State<FeedbackScreen> {
+  final CollectionReference myItems =
+      FirebaseFirestore.instance.collection('feedback');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue.withOpacity(0.15),
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text(
+          'Feedback',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: StreamBuilder(
+        stream: myItems.snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          if (streamSnapshot.hasData) {
+            final items = streamSnapshot.data!.docs;
+            return ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                final DocumentSnapshot documentSnapshot = items[index];
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    // borderRadius: BorderRadius.circular(20),
+                    child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: FeedBackCard(
+                          feedback: documentSnapshot,
+                        )),
+                  ),
+                );
+              },
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(
+              color: Colors.blue,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/ui/widgets/feedback_card.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+
+class FeedBackCard extends StatefulWidget {
+  const FeedBackCard({super.key, required this.feedback});
+  final dynamic feedback;
+
+  @override
+  State<FeedBackCard> createState() => _FeedBackCardState();
+}
+
+class _FeedBackCardState extends State<FeedBackCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 2),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          border: const Border(
+            left: BorderSide(
+              //                   <--- left side
+              color: Colors.blue,
+              width: 6.0,
+            ),
+            top: BorderSide(
+              //                    <--- top side
+              color: Colors.blue,
+              width: 1.0,
+            ),
+            right: BorderSide(
+              //                    <--- top side
+              color: Colors.blue,
+              width: 1.0,
+            ),
+            bottom: BorderSide(
+              //                    <--- top side
+              color: Colors.blue,
+              width: 1.0,
+            ),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.4),
+              spreadRadius: 0,
+              blurRadius: 10,
+            ),
+          ]),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+              padding: const EdgeInsets.only(right: 0.5, left: 0.5),
+              child: Card(
+                color: Colors.white,
+                borderOnForeground: false,
+                shadowColor: Colors.white,
+                child: ListTile(
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.person,
+                            color: Color.fromARGB(255, 147, 148, 148),
+                          ),
+                          const SizedBox(
+                            width: 12,
+                          ),
+                          Expanded(
+                            child: Text(
+                              'Feeback From: ${widget.feedback['user_name'] ?? ''}',
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 16),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.handshake,
+                            color: Color.fromARGB(255, 147, 148, 148),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: Text(
+                              'Title: ${widget.feedback['title'] ?? ''}',
+                              // maxLines: ,
+                              // overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(
+                                fontSize: 14,
+                                height: 1.5,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.description_outlined,
+                            color: Color.fromARGB(255, 147, 148, 148),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: Text(
+                              'Description: ${widget.feedback['description'] ?? ''}',
+                              maxLines: 4,
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(
+                                // fontSize: 14,
+                                // height: 1.5,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const SizedBox(height: 8),
+                    ],
+                  ),
+                ),
+              ))
+        ],
+      ),
+    );
+    ;
+  }
+}
